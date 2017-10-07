23439742baf73d12b7a700bd6007b2fbcc17259a
Michael(Zhiyi Weng) <vyloy.w@gmail.com>
2017-10-07 09:51:42 +0800

    add seed argument

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"flag"
-
 	"os"
 	"os/signal"
+	"path/filepath"
 
 	log "github.com/sirupsen/logrus"
-	"github.com/skycoin/skywire/node"
 	"github.com/skycoin/skycoin/src/util/file"
-	"path/filepath"
+	"github.com/skycoin/skywire/node"
 )
 
 var (
 	managerAddresses node.Addresses
 	address          string
+	// use fixed seed if true
+	seed bool
 	// path for seed, public key and private key
-	seedPath         string
+	seedPath string
 )
 
 func parseFlags() {
 	flag.StringVar(&address, "address", ":5000", "address to listen on")
+	flag.BoolVar(&seed, "seed", true, "use fixed seed to connect if true")
 	flag.StringVar(&seedPath, "seedPath", "", "path to save seed info(default:$HOME/.skywire/node/keys.json)")
 	flag.Var(&managerAddresses, "manager-address", "address of node manager")
 	flag.Parse()
@@ -32,10 +34,15 @@ func main() {
 	osSignal := make(chan os.Signal, 1)
 	signal.Notify(osSignal, os.Interrupt, os.Kill)
 
-	if len(seedPath) < 1 {
-		seedPath = filepath.Join(file.UserHome(), ".skywire", "node", "keys.json")
+	var n *node.Node
+	if !seed {
+		n = node.New("")
+	} else {
+		if len(seedPath) < 1 {
+			seedPath = filepath.Join(file.UserHome(), ".skywire", "node", "keys.json")
+		}
+		n = node.New(seedPath)
 	}
-	n := node.New(seedPath)
 	err := n.Start(managerAddresses, address)
 	if err != nil {
 		log.Error(err)
