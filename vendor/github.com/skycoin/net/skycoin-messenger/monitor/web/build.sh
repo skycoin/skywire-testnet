f6da1b9b51189ca5c6537fdb02c4180b655f3a8e
Michael(Zhiyi Weng) <vyloy.w@gmail.com>
2017-10-24 09:23:19 +0800

    add sh

---
+++ vendor/github.com/skycoin/net/skycoin-messenger/monitor/web/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+source "./tool.sh"
+
+sysOS=`uname -s`
+if [ $sysOS == "Darwin" ];then
+	inMac
+elif [ $sysOS == "Linux" ];then
+	inLinux
+else
+	echo "Other OS: $sysOS"
+    exit 1
+fi
+
+install
+build
---
+++ vendor/github.com/skycoin/net/skycoin-messenger/monitor/web/tool.sh
@@ -0,0 +1,49 @@
+source "./vesrion.sh"
+
+inMac() {
+    if ! type "npm" > /dev/null; then
+        echo "npm is not installed."
+        exit 1c
+    fi
+    compareVesrion "npm -v" 4 "npm"
+    if ! type "ng" > /dev/null; then
+        echo "ng is not installed."
+        exit 1
+    fi
+}
+inLinux() {
+    # Check if curl is installed.
+    if ! dpkg -s npm > /dev/null ; then
+        echo "npm is not installed."
+        exit 1
+    fi
+
+    # Check if jq is installed.
+    if ! dpkg -s ng > /dev/null ; then
+        echo "ng is not installed."
+        exit 1
+    fi
+}
+install() {
+  cmd "npm install"
+}
+dev() {
+  cmd "npm run start"
+}
+
+build(){
+  cmd "npm run build-d"
+}
+
+buildManager(){
+  cmd "npm run build-m"
+}
+cmd() {
+    echo "[ RUNNING ] '${1}' ..."
+    ${1}
+    RETURN_VALUE=$?
+    if [ ${RETURN_VALUE} -ne 0 ]; then
+        err "command '${1}' failed with return value '${RETURN_VALUE}'"
+        exit ${RETURN_VALUE}
+    fi
+}
