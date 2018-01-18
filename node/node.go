b6776568667a48f2863124ccf1dedf81d8d33fa1
Michael(Zhiyi Weng) <vyloy.w@gmail.com>
2018-01-18 17:52:08 +0800

    improve err tips

--- node/node.go
+++ node/node.go
@@ -85,10 +85,14 @@ func (n *Node) Start(discoveries Addresses, address string) (err error) {
 		n.onDiscoveries.Store(addr, false)
 		split := strings.Split(addr, "-")
 		if len(split) != 2 {
-			err = fmt.Errorf("%s is not valid", addr)
+			err = fmt.Errorf("discovery address %s is not valid", addr)
 			return err
 		}
 		tk, err := cipher.PubKeyFromHex(split[1])
+		if err != nil {
+			err = fmt.Errorf("discovery address %s is not valid", addr)
+			return err
+		}
 		err = n.apps.ConnectWithConfig(split[0], &factory.ConnConfig{
 			TargetKey:     tk,
 			Reconnect:     true,
