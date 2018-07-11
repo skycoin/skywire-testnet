614224c86c56e1a1c0dc8972893e5e5d3da16ca8
Marek Mahansky <m.pro.mark@gmail.com>
2018-07-11 15:05:34 +0200

    Add @angular/material

--- static/skywire-manager/src/app/app.module.ts
+++ static/skywire-manager/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './components/pages/login/login.component';
 import { NodeListComponent } from './components/pages/node-list/node-list.component';
 import { NodeComponent } from './components/pages/node/node.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { NodeComponent } from './components/pages/node/node.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
