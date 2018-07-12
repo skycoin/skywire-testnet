a11c253b2d3f35628a0e649439faa775a62c719f
Ivan Payan <ivanpayanm@gmail.com>
2018-07-12 14:31:02 +0200

    Skywire Discovery: Added clipboard.directive.ts

--- static/skywire-discovery/src/app/directives/clipboard/clipboard.directive.ts
+++ static/skywire-discovery/src/app/directives/clipboard/clipboard.directive.ts
@@ -9,7 +9,7 @@ export class ClipboardDirective {
   @Output() copySuccess = new EventEmitter<boolean>();
   cliboard: any = null;
 
-  /*constructor(private el: ElementRef, private render: Renderer) {
+  constructor(private el: ElementRef, private render: Renderer) {
     this.cliboard = new Clipboard(this.el.nativeElement);
     this.render.setElementClass(this.el.nativeElement, 'copy-btn', true);
     this.cliboard.on('success', (e) => {
@@ -19,9 +19,6 @@ export class ClipboardDirective {
       console.error(err);
       this.copySuccess.emit(false);
     });
-  }*/
-  constructor(private el: ElementRef, private render: Renderer) {
-    // FIXME: Update constructor to this
   }
 
   @HostListener('click', ['$event'])
