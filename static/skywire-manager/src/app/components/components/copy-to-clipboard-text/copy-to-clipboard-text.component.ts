ea62d59fe846c525c2f6983e55771d92330aa6e7
Ivan Payan <ivanpayanm@gmail.com>
2018-07-23 17:54:20 +0200

    Added foundation of copy to clipboard text component

---
+++ static/skywire-manager/src/app/components/components/copy-to-clipboard-text/copy-to-clipboard-text.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-copy-to-clipboard-text',
+  templateUrl: './copy-to-clipboard-text.component.html',
+  styleUrls: ['./copy-to-clipboard-text.component.css']
+})
+export class CopyToClipboardTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
