806b8afb3fb0b122a5007ec48ebd772c596eb324
Ivan Payan <ivanpayanm@gmail.com>
2018-07-23 17:52:08 +0200

    Added copy to clipboard lib

--- static/skywire-manager/src/app/components/components/node-apps-list/node-apps-list.component.ts
+++ static/skywire-manager/src/app/components/components/node-apps-list/node-apps-list.component.ts
@@ -1,16 +1,18 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {NodeApp, NodeTransport} from "../../../app.datatypes";
 import {MatTableDataSource} from "@angular/material";
+import * as ClipboardJs from 'clipboard/dist/clipboard.js';
 
 @Component({
   selector: 'node-app-keys-list',
   templateUrl: './node-apps-list.component.html',
   styleUrls: ['./node-apps-list.component.scss']
 })
-export class NodeAppsListComponent implements OnInit
+export class NodeAppsListComponent implements OnInit, OnDestroy
 {
   displayedColumns: string[] = ['index', 'key', 'type'];
   dataSource = new MatTableDataSource<NodeApp>();
+  clipboard: ClipboardJs;
   @Input() apps: NodeApp[] = [];
 
   constructor() { }
@@ -18,6 +20,11 @@ export class NodeAppsListComponent implements OnInit
   ngOnInit()
   {
     this.dataSource.data = this.apps;
+    this.clipboard = new ClipboardJs('.clipBtn');
   }
 
+  ngOnDestroy()
+  {
+    this.clipboard.destroy();
+  }
 }
