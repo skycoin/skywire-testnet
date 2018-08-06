84669215b68c88e80068c6ced0c89fdfeba5023a
Ivan Payan <ivanpayanm@gmail.com>
2018-08-06 17:07:23 +0200

    [WIP] Ssh  Whitelist

--- static/skywire-manager/src/app/components/pages/node/apps/app-sshs/sshs-whitelist/sshs-whitelist.component.ts
+++ static/skywire-manager/src/app/components/pages/node/apps/app-sshs/sshs-whitelist/sshs-whitelist.component.ts
@@ -1,9 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import {AbstractControl, FormControl, FormGroup} from '@angular/forms';
 import {MAT_DIALOG_DATA, MatTableDataSource} from '@angular/material';
 import { AppsService } from '../../../../../../services/apps.service';
-import StringUtils from "../../../../../../utils/stringUtils";
-import {Node} from "../../../../../../app.datatypes";
+import {KeyInputEvent} from "../../../../../layout/key-input/key-input.component";
 
 @Component({
   selector: 'app-sshs-whitelist',
@@ -11,49 +9,43 @@ import {Node} from "../../../../../../app.datatypes";
   styleUrls: ['./sshs-whitelist.component.scss']
 })
 export class SshsWhitelistComponent implements OnInit {
-  form: FormGroup;
   displayedColumns = [ 'index', 'key' ];
   dataSource = new MatTableDataSource<string>();
+  private keyToAdd: string;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) private data: any,
     private appsService: AppsService,
   ) {
-    this.form = new FormGroup({
-      'keys': new FormControl('', [this.validateKeys.bind(this)]),
-    });
-    this.dataSource.data = data.app.allow_nodes;
+    this.dataSource.data = data.app.allow_nodes || [];
   }
 
   save()
   {
-    if (this.form.valid)
-    {
-      this.appsService.startSshServer(this.keysValues()).subscribe();
-    }
+    this.appsService.startSshServer(this.keysValues()).subscribe();
   }
 
-  private validateKeys(control: FormControl)
+  private keysValues(): string[]
   {
-    const isInvalid = this.keysValues(control)
-      .map(key => key.length === 66)
-      .some(result => result === false);
-
-    // Must return null if the form is correct
-    return isInvalid ? { invalid: isInvalid } : null;
-  }
-
-  private get keysInput(): AbstractControl
-  {
-    return this.form.get('keys');
-  }
-
-  private keysValues(control = this.keysInput): string[]
-  {
-    let value = StringUtils.removeWhitespaces((control.value as string));
-    return value.split(',').filter(key => key.length > 0);
+    return this.dataSource.data.concat([]);
   }
 
   ngOnInit(): void {
+  }
+
+  addNodeKey()
+  {
+    let dataCopy = this.keysValues();
+    dataCopy.push(this.keyToAdd);
+
+    this.dataSource.data = dataCopy;
+  }
+
+  onKeyChange({value, valid}: KeyInputEvent)
+  {
+    if (valid)
+    {
+      this.keyToAdd = value;
+    }
   }
 }
