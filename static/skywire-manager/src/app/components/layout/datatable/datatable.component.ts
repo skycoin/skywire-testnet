519868777cdf8894db2f2415f4dbdedd69fd399d
Ivan Payan <ivanpayanm@gmail.com>
2018-08-08 16:50:09 +0200

    WIP: making datatable generic to be able to accept multiple types of input

--- static/skywire-manager/src/app/components/layout/datatable/datatable.component.ts
+++ static/skywire-manager/src/app/components/layout/datatable/datatable.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, Input, OnInit, Output, Type} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, Type, ViewChild} from '@angular/core';
 import {MatTableDataSource} from "@angular/material";
+import {KeyInputComponent} from "../key-input/key-input.component";
 
 @Component({
   selector: 'app-datatable',
@@ -8,28 +9,29 @@ import {MatTableDataSource} from "@angular/material";
 })
 export class DatatableComponent implements OnInit
 {
-  private dataSource = new MatTableDataSource<string>();
+  dataSource = new MatTableDataSource<string>();
 
   @Input() data: string[];
   @Output() onSave: EventEmitter<string[]>;
 
   // Header
-  @Input() displayedColumns: string[];
-  @Input() headerTitle: string;
-
-  // Remove row section
-  @Input() removeRowTooltipText: string;
+  displayedColumns = [ 'index', 'key', 'remove' ];
 
   // Editabe rows
-  @Input() getEditableRowData: any;
-  @Input() getEditableRowComponentClass: Type<any>;
+  @Input() getEditableRowData: (index: number, currentValue: string, callback: ()=>any) => any;
+  @Input() getEditableRowComponentClass: () => Type<any>;
 
   // Add input section
-  @Input() getAddRowData: any;
-  @Input() getAddRowComponentClass: Type<any>;
-  @Input() addButtonTitle: string;
-  @Input() onAddBtnClicked: () => void;
-  @Input() valueToAdd: string;
+  @Input() getAddRowData: (any) => any;
+  @Input() getAddRowComponentClass: () => Type<any>;
+
+  @Input() meta: {
+    headerTitle: string,
+    removeRowTooltipText: string,
+    addButtonTitle: string
+  };
+
+  valueToAdd: string;
 
   constructor() { }
 
@@ -41,6 +43,38 @@ export class DatatableComponent implements OnInit
   private updateValues(list: string[])
   {
     this.dataSource.data = list;
-    this.onSave.emit(list);
+    if (this.onSave)
+    {
+      this.onSave.emit(list);
+    }
+  }
+
+  onAddValueChanged({value, valid}): void
+  {
+    if (valid)
+    {
+      this.valueToAdd = value;
+    }
+  }
+
+  onAddBtnClicked()
+  {
+    this.data.push(this.valueToAdd);
+    this.updateValues(this.data);
+
+    this.valueToAdd = null;
+  }
+
+  onRemoveBtnClicked(position)
+  {
+    this.data.splice(position, 1);
+    this.updateValues(this.data);
+  }
+
+  onKeyAtPositionChanged(position: number, keyValue: string)
+  {
+    let dataCopy = this.data;
+    dataCopy[position] = keyValue;
+    this.updateValues(dataCopy);
   }
 }
--- static/skywire-manager/src/app/components/pages/node/apps/app-sshs/sshs-whitelist/sshs-whitelist.component.ts
+++ static/skywire-manager/src/app/components/pages/node/apps/app-sshs/sshs-whitelist/sshs-whitelist.component.ts
@@ -1,7 +1,7 @@
-import {Component, ComponentFactoryResolver, EventEmitter, Inject, OnInit, ViewChild} from '@angular/core';
-import {MAT_DIALOG_DATA, MatTableDataSource} from '@angular/material';
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material';
 import { AppsService } from '../../../../../../services/apps.service';
-import {KeyInputComponent, KeyInputEvent} from "../../../../../layout/key-input/key-input.component";
+import {KeyInputComponent} from "../../../../../layout/key-input/key-input.component";
 import {EditableKeyComponent} from "../../../../../layout/editable-key/editable-key.component";
 
 @Component({
@@ -9,67 +9,17 @@ import {EditableKeyComponent} from "../../../../../layout/editable-key/editable-
   templateUrl: './sshs-whitelist.component.html',
   styleUrls: ['./sshs-whitelist.component.scss']
 })
-export class SshsWhitelistComponent implements OnInit
+export class SshsWhitelistComponent
 {
-  displayedColumns = [ 'index', 'key', 'remove' ];
-  private valueToAdd: string;
-
-  @ViewChild(KeyInputComponent) newKeyInput: KeyInputComponent;
-  removeRowTooltipText: string = "Remove key";
-  addButtonTitle: string = "Add to list";
-
-  onKeyAtPositionChanged(position: number, keyValue: string)
-  {
-    let dataCopy = this.data;
-    dataCopy[position] = keyValue;
-    this.data = dataCopy;
-    this.save(dataCopy);
-  }
-
   constructor(
     @Inject(MAT_DIALOG_DATA) private data: any,
     private appsService: AppsService
   ) {
-    this.updateKeys(data.app.allow_nodes || []);
   }
 
   save(keys: string[])
   {
     this.appsService.startSshServer(keys).subscribe();
-  }
-
-  ngOnInit(): void {
-  }
-
-  onAddBtnClicked()
-  {
-    let dataCopy = this.data;
-    dataCopy.push(this.valueToAdd);
-    this.updateKeys(dataCopy);
-
-    this.newKeyInput.clear();
-    this.valueToAdd = null;
-  }
-
-  onAddRowValueChange({value, valid}: KeyInputEvent)
-  {
-    if (valid)
-    {
-      this.valueToAdd = value;
-    }
-  }
-
-  onRemoveBtnClicked(position)
-  {
-    let keys = this.data;
-    keys.splice(position, 1);
-    this.updateKeys(keys);
-  }
-
-  private updateKeys(keys: string[])
-  {
-    this.data = keys;
-    this.save(keys);
   }
 
   getEditableRowComponentClass() {
@@ -80,21 +30,21 @@ export class SshsWhitelistComponent implements OnInit
     return KeyInputComponent;
   }
 
-  getAddRowData()
+  getAddRowData(addRowCallback)
   {
     return {
       required: false,
       placeholder: 'Enter node key',
-      onKeyChangeSubscriber: this.onAddRowValueChange.bind(this)
+      onKeyChangeSubscriber: addRowCallback
     }
   }
 
-  getEditableRowData(index: number)
+  getEditableRowData(index: number, currentValue: string, rowChangeCallback)
   {
     return {
       autofocus: true,
-      value: this.data[index],
-      subscriber: this.onKeyAtPositionChanged.bind(this, index)
+      value: currentValue,
+      subscriber: (value) => rowChangeCallback(index, value)
     }
   }
 }
