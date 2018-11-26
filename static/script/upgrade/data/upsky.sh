957c897962347892baf632f3452cbf67f89a0260
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2018-11-26 09:11:32 -0500

    Refs: #171 Final change: point github refferences to skycoin

--- static/script/upgrade/data/upsky.sh
+++ static/script/upgrade/data/upsky.sh
@@ -11,9 +11,7 @@ PATH="/usr/local/go/bin:/usr/local/skywire/go/bin:${PATH}"
 SKYCOIN_DIR=${GOPATH}/src/github.com/skycoin
 SKYWIRE_DIR=${SKYCOIN_DIR}/skywire
 SKYWIRE_SCRIPTS=${SKYWIRE_DIR}/static/script/
-# TODO restore to official previous to the merge
-#SKYWIRE_GIT_URL="https://github.com/skycoin/skywire.git"
-SKYWIRE_GIT_URL="https://github.com/simelo/skywire.git"
+SKYWIRE_GIT_URL="https://github.com/skycoin/skywire.git"
 
 export HOME
 export GOPATH
