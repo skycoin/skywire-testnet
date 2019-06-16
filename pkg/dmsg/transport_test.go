dcaba2a807b395f64434e2c131f29f21fbb33297
Sir Darkrengarius <darkren92@gmail.com>
2019-06-16 18:39:59 +0300

    Fix tests broke after merge

--- pkg/dmsg/transport_test.go
+++ pkg/dmsg/transport_test.go
@@ -1,8 +1,6 @@
 package dmsg
 
-/*import (
-	"context"
-	"errors"
+import (
 	"testing"
 
 	"github.com/skycoin/skycoin/src/util/logging"
@@ -11,17 +9,15 @@ package dmsg
 	"github.com/skycoin/skywire/pkg/cipher"
 )
 
-var (
-	log = logging.MustGetLogger("dmsg_test")
-)
-
 func TestNewTransport(t *testing.T) {
-	tr := NewTransport(nil, log, cipher.PubKey{}, cipher.PubKey{}, 0)
+	log := logging.MustGetLogger("dmsg_test")
+	tr := NewTransport(nil, log, cipher.PubKey{}, cipher.PubKey{}, 0, func(id uint16) {})
 	assert.NotNil(t, tr)
 }
 
 func TestTransport_close(t *testing.T) {
-	tr := NewTransport(nil, log, cipher.PubKey{}, cipher.PubKey{}, 0)
+	log := logging.MustGetLogger("dmsg_test")
+	tr := NewTransport(nil, log, cipher.PubKey{}, cipher.PubKey{}, 0, func(id uint16) {})
 
 	closed := tr.close()
 
@@ -30,7 +26,7 @@ func TestTransport_close(t *testing.T) {
 	})
 
 	t.Run("Channel closed (1st attempt)", func(t *testing.T) {
-		_, ok := <-tr.doneCh
+		_, ok := <-tr.done
 		assert.False(t, ok)
 	})
 
@@ -41,46 +37,7 @@ func TestTransport_close(t *testing.T) {
 	})
 
 	t.Run("Channel closed (2nd attempt)", func(t *testing.T) {
-		_, ok := <-tr.doneCh
+		_, ok := <-tr.done
 		assert.False(t, ok)
 	})
 }
-
-func TestTransport_awaitResponse(t *testing.T) {
-	tr := NewTransport(nil, log, cipher.PubKey{}, cipher.PubKey{}, 0)
-
-	t.Run("Request rejected", func(t *testing.T) {
-		go func() {
-			tr.doneCh <- struct{}{}
-		}()
-
-		err := tr.awaitResponse(context.TODO())
-		assert.Equal(t, ErrRequestRejected, err)
-	})
-
-	t.Run("Context cancelled", func(t *testing.T) {
-		ctx, cancel := context.WithCancel(context.TODO())
-		cancel()
-		err := tr.awaitResponse(ctx)
-		assert.Equal(t, context.Canceled, err)
-	})
-
-	t.Run("Invalid remote response", func(t *testing.T) {
-		go func() {
-			tr.readCh <- MakeFrame(RequestType, 1, nil)
-		}()
-
-		err := tr.awaitResponse(context.TODO())
-		assert.Equal(t, errors.New("invalid remote response"), err)
-	})
-
-	t.Run("Valid remote response", func(t *testing.T) {
-		go func() {
-			tr.readCh <- MakeFrame(AcceptType, 1, nil)
-		}()
-
-		err := tr.awaitResponse(context.TODO())
-		assert.NoError(t, err)
-	})
-}
-*/
