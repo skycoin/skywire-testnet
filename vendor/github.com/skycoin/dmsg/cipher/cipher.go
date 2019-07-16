14bce2ed64e3fb4115e6045893b846289ee56703
Sir Darkrengarius <darkren92@gmail.com>
2019-07-16 10:49:00 +0300

    Update vendor

--- vendor/github.com/skycoin/dmsg/cipher/cipher.go
+++ vendor/github.com/skycoin/dmsg/cipher/cipher.go
@@ -3,6 +3,7 @@
 package cipher
 
 import (
+	"bytes"
 	"fmt"
 	"strings"
 
@@ -72,6 +73,10 @@ func (pk PubKey) MarshalText() ([]byte, error) {
 
 // UnmarshalText implements encoding.TextUnmarshaler.
 func (pk *PubKey) UnmarshalText(data []byte) error {
+	if bytes.Count(data, []byte{48}) == len(data) {
+		return nil
+	}
+
 	dPK, err := cipher.PubKeyFromHex(string(data))
 	if err == nil {
 		*pk = PubKey(dPK)
