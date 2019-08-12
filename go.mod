fca2d56e2ca80df746997e7f70fd8db17d05190a
Alex Yu <alexander.yuryshev@gmail.com>
2019-08-12 20:14:51 +0300

    Start of multihead test

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.12
 
 require (
 	github.com/armon/go-socks5 v0.0.0-20160902184237-e75332964ef5
+	github.com/blang/semver v3.5.1+incompatible // indirect
+	github.com/boltdb/bolt v1.3.1 // indirect
 	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/handlers v1.4.0
@@ -14,6 +16,7 @@ require (
 	github.com/pkg/profile v1.3.0
 	github.com/prometheus/client_golang v1.0.0
 	github.com/prometheus/common v0.4.1
+	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24 // indirect
 	github.com/sirupsen/logrus v1.4.2
 	github.com/skycoin/dmsg v0.0.0-20190805065636-70f4c32a994f
 	github.com/skycoin/skycoin v0.26.0
---
+++ pkg/visor/visor_multihead_test.go
@@ -0,0 +1,145 @@
+// +build !no_ci
+
+package visor
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/skycoin/dmsg/cipher"
+	"github.com/skycoin/skywire/pkg/routing"
+)
+
+func readConfig(cfgFile string) (Config, error) {
+	rdr, err := os.Open(filepath.Clean(cfgFile))
+	if err != nil {
+		return Config{}, err
+	}
+	conf := Config{}
+	if err := json.NewDecoder(rdr).Decode(&conf); err != nil {
+		return Config{}, err
+	}
+	return conf, err
+}
+
+func ipPool(addrTemplate string, n uint) []string {
+	ipAddrs := make([]string, n)
+	for i := uint(1); i < n+1; i++ {
+		ipAddrs[i-1] = fmt.Sprintf(addrTemplate, i)
+	}
+	return ipAddrs
+}
+
+func Example_ipPool() {
+
+	ipAddrs := ipPool("12.12.12.%d", 1)
+	fmt.Printf("%v\n", ipAddrs)
+
+	// Output: ZZZZ
+}
+
+func cfgPool(baseCfg Config, ipTmplt, localPathTmplt string, n uint) []Config {
+	nodeCfgs := make([]Config, n)
+
+	for i := uint(0); i < n; i++ {
+		ip := fmt.Sprintf(ipTmplt, i+1)
+		localPath := fmt.Sprintf(localPathTmplt, i+1)
+		pk, sk, _ := cipher.GenerateDeterministicKeyPair([]byte(ip))
+
+		baseCfg.Node.StaticPubKey = pk
+		baseCfg.Node.StaticSecKey = sk
+		baseCfg.LocalPath = localPath
+		baseCfg.Interfaces.RPCAddress = fmt.Sprintf("%s:3435", ip)
+		baseCfg.Transport.LogStore.Location = fmt.Sprintf("%s/transport_logs", localPath)
+		baseCfg.Routing.Table.Location = fmt.Sprintf("%s/routing.db", localPath)
+		baseCfg.Apps = []AppConfig{
+			AppConfig{
+				App:       "skychat",
+				AutoStart: true,
+				Port:      routing.Port(1),
+				Args: []string{
+					"-addr",
+					fmt.Sprintf("%v:8001", ip),
+				},
+			}}
+		baseCfg.TCPTransportAddr = fmt.Sprintf("%v:9119", ip)
+
+		nodeCfgs[i] = baseCfg
+	}
+	return nodeCfgs
+}
+
+func Example_cfgPool() {
+	cfgFile, err := filepath.Abs("../../integration/tcp-tr/nodeA.json")
+	baseCfg, err := readConfig(cfgFile)
+	fmt.Printf("readConfig success: %v\n", err == nil)
+
+	nodeCfgs := cfgPool(baseCfg, "12.12.12.%d", "./local/node_%03d", 3)
+	fmt.Printf("len(nodeCfgs): %v\n", len(nodeCfgs))
+
+	// Output: readConfig success: true
+	// len(nodeCfgs): 3
+}
+
+func nodePool(cfgs []Config) []*Node {
+	nodes := make([]*Node, len(cfgs))
+	// loggers := make([])
+
+	var err error
+	for i := 0; i < len(cfgs); i++ {
+		nodes[i], err = NewNode(&cfgs[i], masterLogger)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return nodes
+}
+
+/* Run in shell:
+$ for ((i=1; i<=16; i++)){_ ip addr add 12.12.12.$i/32 dev lo}
+*/
+
+func Example_nodePool() {
+	cfgFile, err := filepath.Abs("../../integration/tcp-tr/nodeA.json")
+	baseCfg, err := readConfig(cfgFile)
+	baseCfg.PubKeysFile, _ = filepath.Abs("../../integration/tcp-tr/hosts.pubkeys")
+
+	fmt.Printf("readConfig success: %v\n", err == nil)
+	nodeCfgs := cfgPool(baseCfg, "12.12.12.%d", "./local/node_%03d", 16)
+
+	nodes := nodePool(nodeCfgs)
+	fmt.Printf("Nodes: 12.12.12.1 - 12.12.12.%d", len(nodes))
+
+	// Output: readConfig success: true
+	// Nodes: 12.12.12.1 - 12.12.12.16
+}
+
+func startMultiHead(nodes []*Node) []error {
+	errs := []error{}
+	for i := 0; i < len(nodes); i++ {
+		if err := nodes[i].Start(); err != nil {
+			errs = append(errs, fmt.Errorf("error %v starting node %v", err, i))
+		}
+	}
+	return errs
+}
+
+func Example_startMultiHead() {
+	cfgFile, err := filepath.Abs("../../integration/tcp-tr/nodeA.json")
+	baseCfg, err := readConfig(cfgFile)
+	baseCfg.PubKeysFile, _ = filepath.Abs("../../integration/tcp-tr/hosts.pubkeys")
+	fmt.Printf("baseCfg success: %v\n", err == nil)
+
+	nodeCfgs := cfgPool(baseCfg, "12.12.12.%d", "./local/node_%03d", 1)
+	nodes := nodePool(nodeCfgs)
+
+	_ = nodes
+	var errs []error
+	errs = startMultiHead(nodes)
+
+	fmt.Printf("errors: %v\n", errs)
+
+	// Output: ZZZZ
+}
