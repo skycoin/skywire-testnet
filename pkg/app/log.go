9ca33f9ce8f97833f20c73382bb2b2362bea6b84
ivcosla <ivcoslaj@gmail.com>
2019-08-20 13:52:43 +0200

    added bbolt fetch logs

--- pkg/app/log.go
+++ pkg/app/log.go
@@ -1,13 +1,24 @@
 package app
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"time"
 
 	"go.etcd.io/bbolt"
 )
 
+// LogStore stores logs from apps, for later consumption from the hypervisor
 type LogStore interface {
+
+	// Store saves given log in db
+	Store(t time.Time, string) error
+
+	// LogSince returns the logs since given timestamp. For optimal performance,
+	// the timestamp should exist in the store (you can get it from previous logs),
+	// otherwise the DB will be sequentially iterated until finding entries older than given timestamp
+	LogsSince(t time.Time) ([]string, error)
 }
 
 type boltDBappLogs struct {
@@ -36,6 +47,40 @@ func newBoltDB(path, appName string) (LogStore, error) {
 	return &boltDBappLogs{db, b}, nil
 }
 
-func (l *boltDBappLogs) LogsSince(time time.Time) ([]string, error) {
-	err := l.db.View()
+// LogSince implements LogStore
+func (l *boltDBappLogs) LogsSince(t time.Time) ([]string, error) {
+	logs := make([]string, 0)
+
+	err := l.db.View(func(tx *bbolt.Tx) error {
+		b := tx.Bucket(l.bucket)
+
+		c := b.Cursor()
+		if k, _ := c.Seek([]byte(t.Format(time.RFC3339))); k != nil {
+			iterateFromKey(c, logs)
+		} else {
+			iterateFromBeginning(c, t, logs)
+		}
+
+		return nil
+	})
+
+	return logs, err
+}
+
+func iterateFromKey(c *bbolt.Cursor, logs []string) {
+	for k, v := c.Next(); k != nil; k, v = c.Next() {
+		logs = append(logs, fmt.Sprintf("%s-%s", string(k), string(v)))
+	}
+}
+
+func iterateFromBeginning(c *bbolt.Cursor, t time.Time, logs []string) {
+	parsedT := []byte(t.UTC().Format(time.RFC3339))
+
+	for k, v := c.First(); k != nil; k, v = c.Next() {
+		if bytes.Compare(parsedT, k) < 0 {
+			continue
+		}
+
+		logs = append(logs, fmt.Sprintf("%s-%s", string(k), string(v)))
+	}
 }
