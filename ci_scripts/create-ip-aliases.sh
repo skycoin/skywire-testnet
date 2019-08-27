80c724dfdc0a466353ef4cb0a09b4a1548cc3303
Alex Yu <alexander.yuryshev@gmail.com>
2019-08-27 14:42:32 +0300

    second attempt to create ip aliases in travis

--- ci_scripts/create-ip-aliases.sh
+++ ci_scripts/create-ip-aliases.sh
@@ -3,11 +3,11 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then     
     for ((i=1; i<=255; i++)) 
     do 
-        ip addr add 12.12.12.$i/32 dev lo 
+        sudo ip addr add 12.12.12.$i/32 dev lo 
     done
 elif [[ "$OSTYPE" == "darwin" ]]; then 
     for ((i=1; i<=255; i++))
     do 
-    ip addr add 12.12.12.$i/32 dev lo0
+        sudo ip addr add 12.12.12.$i/32 dev lo0
     done
 fi
