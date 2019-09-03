d475fc27eeb2b1da1de27f10546b8340c929e643
Alex Yu <alexander.yuryshev@gmail.com>
2019-09-03 09:57:27 +0300

    Vendor integration tests now goes with skywire version from filesystem

--- ci_scripts/vendor-integration-check.sh
+++ ci_scripts/vendor-integration-check.sh
@@ -4,12 +4,18 @@ export GO111MODULE=on
 export COMPOSE_FILE=docker-compose.yml:docker-compose.nodes.yml
 set -e -x
 
+# Store current path
+export SWPATH=$(pwd)
+
 # Clone from master
 rm -rf /tmp/skywire-services &> /dev/null
 cd /tmp
 git clone https://$GITHUB_TOKEN:x-oauth-basic@github.com/watercompany/skywire-services.git  --branch master --depth 1
 # git clone git@github.com:watercompany/skywire-services.git --branch master --depth 1
 cd skywire-services
+
+# go mod edit
+go mod edit  -replace=github.com/skycoin/skywire@mainnet=$SWPATH
 
 # Checking build 
 make dep
